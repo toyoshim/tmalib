529c820920e709a836038469bb67eb109269279a
Takashi Toyoshima <toyoshim@gmail.com>
2013-12-23 23:42:38 +0900

    Add MajVj extension with wired frame plugin and a simple example

---
+++ ext/mv/MajVj.js
@@ -0,0 +1,170 @@
+/**
+ * T'MediaArt library for JavaScript.
+ *  - MajVj extension -
+ */
+
+ /**
+  * MajVj prototype
+  *
+  * This prototype provides a framework for VJ applications.
+  * @author Takashi Toyoshima <toyoshim@gmail.com>
+  * @param width offscreen width
+  * @param height offscreen height
+  */
+function MajVj (width, height) {
+    this._width = width;
+    this._height = height;
+    this._aspect = height / width;
+    this._timestamp = 0;
+    this._screen = new TmaScreen(width, height, TmaScreen.MODE_3D);
+    this._screen.setAlphaMode(
+            true, this._screen.gl.SRC_ALPHA, this._screen.gl.ONE);
+    this._screen.attachTo(TmaScreen.BODY);
+    this.onresize();
+}
+
+/**
+ * Loads a MajVj plugin.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @return a Promise object
+ */
+MajVj.prototype.loadPlugin = function (type, name) {
+    return new Promise(function (resolve, reject) {
+        MajVj.loadScript(type, name, 'main.js').then(function () {
+            MajVj[type][name].load().then(function () {
+                resolve();
+            }, function (e) { reject(e); });
+        }, function () { reject('plugin load error: ' + name); });
+    });
+};
+
+/**
+ * Loads all MajVj plugins.
+ * @return a Promise object
+ */
+MajVj.prototype.loadAllPlugins = function () {
+    return Promise.all([
+        this.loadPlugin('frame', 'wired')
+    ]);
+};
+
+/**
+ * Handles screen resize.
+ */
+ MajVj.prototype.onresize = function () {
+    this._screen.canvas.style.width = window.innerWidth + 'px';
+    this._screen.canvas.style.height = window.innerHeight + 'px';
+    this._aspect =
+            this._screen.canvas.clientWidth / this._screen.canvas.clientHeight;
+    console.log('resize: ' + this._screen.canvas.clientWidth + 'x' +
+            this._screen.canvas.clientHeight);
+};
+
+/**
+ * Creates a plugin instance.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @return a plugin instance
+ */
+MajVj.prototype.create = function (type, name) {
+    return new MajVj[type][name](
+            this._screen, this._width, this._height, this._aspect);
+};
+
+/**
+ * Returns using Tma3DScreen object.
+ * return Tma3DScreen object
+ */
+MajVj.prototype.screen = function () {
+    return this._screen;
+};
+
+/**
+ * Run the main function periodically.
+ * @param main a function to run periodically
+ */
+ MajVj.prototype.run = function (main) {
+    var requestAnimationFrame = window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame;
+    var loop = function (time) {
+        var delta = time - this._timestamp;
+        this._timestamp = time;
+        main(delta);
+        requestAnimationFrame(loop, this._canvas);
+    }.bind(this);
+    this._timestamp = window.mozAnimationStartTime || Date.now();
+    this._timestamp -= 0.1;
+    loop(this._timestamp + 0.1);
+};
+
+/**
+ * Creates source path for plugins with type and name.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @param path relative file path in plugin directory
+ * @return a path name in text
+ */
+MajVj.createPath = function (type, name, path) {
+    return tma.basePath() + 'ext/mv/' + type + '/' + name + '/' + path;
+};
+
+/**
+ * Loads shader program from an external html file.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @param html path for an external html file
+ * @param id html tag id
+ * @return a Promise object
+ */
+MajVj.loadShader = function (type, name, html, id) {
+    return tma.loadShader(MajVj.createPath(type, name, html), id);
+};
+
+/**
+ * Loads an external JavaScript file.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @param src relative file path in plugin directory
+ * @return a Promise object
+ */
+MajVj.loadScript = function (type, name, src) {
+    return tma.load(MajVj.createPath(type, name, src));
+};
+
+/**
+ * Loads an image file.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @param path relative file path in plugin directory
+ * @return a Promise object
+ */
+ MajVj.loadImage = function (type, name, path) {
+    return new Promise(function (resolve, reject) {
+        var image = new Image();
+        image.onload = function () { resolve(this); };
+        image.src = MajVj.createPath(type, name, path);
+    });
+};
+
+/**
+ * Loads a movie file.
+ * @param type 'effect' or 'frame'
+ * @param name plugin name
+ * @param path relative file path in plugin directory
+ * @return a Promise object
+ */
+MajVj.loadMovie = function (type, name, path) {
+    return new Promise(function (resolve, reject) {
+        var video = document.createElement('video');
+        video.src = createPath(type, name, path);
+        video.style.setProperty('display', 'none');
+        video.addEventListener('canplay', function () {
+            resolve(this);
+        }, false);
+        document.head.appendChild(video);
+    });
+};
+
+MajVj.effect = {};
+MajVj.frame = {};
---
+++ ext/mv/frame/wired/main.js
@@ -0,0 +1,112 @@
+/**
+ * T'MediaArt library for JavaScript
+ *  - MajVj extension - frame plugin - wired -
+ * @param screen Tma3DScreen object
+ * @param width offscreen width
+ * @param height offscreen height
+ * @param aspect screen aspect ratio (screen height / screen width)
+ */
+MajVj.frame.wired = function (screen, width, height, aspect) {
+    this._screen = screen;
+    this._width = width;
+    this._height = height;
+    this._aspect = aspect;
+    this._controller = null;
+    this._rotate = 0.0;
+
+    this._program = screen.createProgram(
+            screen.compileShader(Tma3DScreen.VERTEX_SHADER,
+                    MajVj.frame.wired._vertexShader),
+            screen.compileShader(Tma3DScreen.FRAGMENT_SHADER,
+                    MajVj.frame.wired._fragmentShader));
+    this._lines = screen.createBuffer(function () {
+        var i = 0;
+        var lines = new Array()
+        for (var z = -900; z <= 900; z += 100) {
+            for (var x = -900; x <= 900; x += 100) {
+                lines[i + 0] = x;
+                lines[i + 1] = -900;
+                lines[i + 2] = z;
+                lines[i + 3] = x;
+                lines[i + 4] = 900;
+                lines[i + 5] = z;
+                i += 6;
+            }
+            for (var y = -900; y <= 900; y += 100) {
+                lines[i + 0] = -900;
+                lines[i + 1] = y;
+                lines[i + 2] = z;
+                lines[i + 3] = 900;
+                lines[i + 4] = y;
+                lines[i + 5] = z;
+                i += 6;
+            }
+        }
+        for (var x = -900; x <= 900; x += 100) {
+            for (var y = -900; y <= 900; y += 100) {
+                lines[i + 0] = x;
+                lines[i + 1] = y;
+                lines[i + 2] = -900;
+                lines[i + 3] = x;
+                lines[i + 4] = y;
+                lines[i + 5] = 900;
+                i += 6;
+            }
+        }
+        return lines;
+    } ());
+    this._lines.items = 1944;
+    this._pMatrix = mat4.create();
+    this.onresize(aspect);
+};
+
+/**
+ * Loads resources asynchronously.
+ * @return a Promise object
+ */
+MajVj.frame.wired.load = function () {
+    return new Promise(function (resolve, reject) {
+        Promise.all([
+            MajVj.loadShader('frame', 'wired', 'shaders.html', 'vertex'),
+            MajVj.loadShader('frame', 'wired', 'shaders.html', 'fragment')
+        ]).then(function (shaders) {
+            MajVj.frame.wired._vertexShader = shaders[0];
+            MajVj.frame.wired._fragmentShader = shaders[1];
+            resolve();
+        }, function () { reject('wired.load fails'); });
+    });
+};
+
+/**
+ * Handles screen resize.
+ * @param aspect screen aspect ratio
+ */
+MajVj.frame.wired.prototype.onresize = function (aspect) {
+    this._aspect = aspect;
+    mat4.perspective(45, aspect, 0.1, 1000.0, this._pMatrix);
+    mat4.translate(this._pMatrix, [ 0.0, 0.0, -250.0 ]);
+    mat4.rotate(this._pMatrix, this._rotate, [ 0.1, 0.2, 0.0 ]);
+};
+
+/**
+ * Draws a frame.
+ * @param delta delta time from the last rendering
+ */
+MajVj.frame.wired.prototype.draw = function (delta) {
+    var rotate = 0.002 * delta;
+    if (this._controller)
+        rotate = rotate * (0.5 + this._controller.slider * 1.5);
+    this._rotate += rotate;
+    mat4.rotate(this._pMatrix, rotate, [ 0.1, 0.2, 0.0 ]);
+    this._program.setUniformMatrix('uPMatrix', this._pMatrix);
+    this._program.setAttributeArray(
+            'aVertexPosition', this._lines, 0, 3, 0);
+    this._program.drawArrays(Tma3DScreen.MODE_LINES, 0, this._lines.items);
+};
+
+/**
+ * Attaches a controller.
+ */
+MajVj.frame.wired.prototype.setController = function (controller) {
+    this._controller = controller;
+};
