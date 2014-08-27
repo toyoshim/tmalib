8ab38826ebafdaac3b373e3a35b690ee30a14df5
Takashi Toyoshima <toyoshim@gmail.com>
2014-08-27 17:54:20 +0900

    MajVj sound misc: comment typo fix

--- ext/mv/misc/sound/main.js
+++ ext/mv/misc/sound/main.js
@@ -1,6 +1,6 @@
 /**
  * T'MediaArt library for JavaScript
- *  - MajVj extension - frame plugin - wired -
+ *  - MajVj extension - misc plugin - sound -
  * @param options options (See MajVj.prototype.create)
  */
 MajVj.misc.sound = function (options) {
