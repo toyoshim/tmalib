a3085c9eb862c96f8badfcab62e148714b916ed3
Takashi Toyoshima <toyoshim@gmail.com>
2014-10-19 23:25:25 +0900

    Split core utilities from tma.js to src/core.js
    tma.js keeps compatibility, but tma.js depends much on src/core.js so
    that tma.js specific code is minimized.
    
    Some part of core utilities are rewritten to use Promise. It results in
    loading multiple scripts in parallel with minimal efforts. It reduce
    loading time by almost 50%.

---
+++ src/core.js
@@ -0,0 +1,172 @@
+/**
+ * T'MediaArt library for JavaScript.
+ * - core script - the second boot loader.
+ */
+
+var exports = {};
+
+/**
+ * Takes a log at a each level.
+ * @param arguments Variable number of arguments to log
+ */
+tma.log = function () {
+    console.log.apply(console, arguments);
+};
+tma.info = function () {
+    console.info.apply(console, arguments);
+};
+tma.warn = function () {
+    console.warn.apply(console, arguments);
+};
+tma.error = function () {
+    console.error.apply(console, arguments);
+};
+// Error callback.
+tma.ecb = function (e) {
+  console.error(e.stack);
+};
+
+// Holds loading and loaded JavaScript libraries.
+tma._libraries = {};
+
+/**
+ * Loads JavaScript library asynchronously.
+ * @param src a source file URL
+ * @param callback callback to invoke when the JavaScript is loaded (optional)
+ * @return Promise if callback is not specified
+ */
+tma.load = function (src, callback) {
+  if (!tma._libraries[src]) {
+    // This is the first request to |src|?
+    var promise = new Promise(function (resolve, reject) {
+      var script = document.createElement('script');
+      var state = {
+        callbacks: [resolve],
+        ready: false,
+        script: script
+      };
+      if (callback)
+        state.callbacks.push(callback);
+      tma._libraries[src] = state;
+      script.src = src;
+      script.onload = function () {
+        this.ready = true;
+        for (var i = 0; i < this.callbacks.length; ++i)
+          this.callbacks[i]();
+        this.callbacks = undefined;
+      }.bind(state);
+      document.head.appendChild(script);
+    });
+    return promise;
+  } else if (tma._libraries[src].ready) {
+    // The same library is already loaded.
+    if (callback)
+      callback();
+    return new Promise(function (resolve, reject) {
+      resolve();
+    });
+  } else {
+    // The same library is on loading.
+    var promise = new Promise(function (resolve, reject) {
+      var callbacks = tma._libraries[src].callbacks;
+      callbacks.push(resolve);
+      if (callback)
+        callbacks.push(callback);
+    });
+    return promise;
+  }
+};
+
+/**
+ * TODO: Add a cache capability.
+ * Fetches a data via XMLHttpRequest.
+ * @param url a url to fetch a data
+ * @param type a response type in string (optional: 'arraybuffer' is default)
+ * @return a Primise
+ */
+tma.fetch = function (url, type) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = type || 'arraybuffer';
+    xhr.onload = function () {
+      if (!this.response)
+        reject();
+      else
+        resolve(this.response);
+      this.onload = null;
+    }.bind(xhr);
+    xhr.send();
+  });
+};
+
+/**
+ * TODO: Use fetch with resopnseType 'document'.
+ * Loads a shader program from external html by id.
+ * @param src an external html URL
+ * @param id script ID to obtain the shader program
+ * @return Promise
+ */
+tma.loadShader = function (src, id) {
+  return new Promise(function (resolve, reject) {
+    var frames = document.getElementsByTagName('iframe');
+    for (var i = 0; i < frames.length; ++i) {
+      if (frames[i].src == src || frames[i]._src == src) {
+        if (frames[i]._load) {
+          resolve(frames[i].contentDocument.getElementById(id).text);
+        } else {
+          var callback = frames[i].onload;
+          frames[i].onload = function () {
+            callback.apply(this);
+            resolve(frames[i].contentDocument.getElementById(id).text);
+          };
+        }
+        return;
+      }
+    }
+    var frame = document.createElement('iframe');
+    frame.onload = function () {
+      this._load = true;
+      resolve(this.contentDocument.getElementById(id).text);
+    };
+    frame.src = src;
+    frame._src = src;
+    tma._head.appendChild(frame);
+  });
+};
+
+/**
+ * Loads sub scripts.
+ */
+tma._boot = function () {
+  // TmaModelPromitives depends on Tma3DScreen.
+  // Tma3DScreen and Tma2DScreen depend on TmaScreen.
+  var screen = new Promise(function (resolve, reject) {
+    tma.load(tma.base + 'src/TmaScreen.js').then(function () {
+      Promise.all([
+        tma.load(tma.base + 'src/Tma2DScreen.js'),
+        new Promise(function (resolve, reject) {
+          tma.load(tma.base + 'src/Tma3DScreen.js', function () {
+            tma.load(tma.base + 'src/TmaModelPrimitives.js', resolve);
+          }, tma.ecb);
+        }, tma.ecb)
+      ]).then(function () { resolve(); }, tma.ecb);
+    }, tma.ecb);
+  });
+  Promise.all([
+    screen,
+    tma.load(tma.base + 'src/TmaParticle.js'),
+    tma.load(tma.base + 'src/TmaSequencer.js'),
+    tma.load(tma.base + 'src/TmaMotionBvh.js'),
+    tma.load(tma.base + 'src/TmaModelPly.js'),
+    tma.load(tma.base + 'src/TmaModelPs2Ico.js')
+  ]).then(function () {
+    tma.ready = true;
+    Promise.all(tma.extlibs.map(function (src) {
+      return tma.load(src);
+    })).then(function () {
+      if (tma.onload)
+        tma.onload();
+    }, tma.ebc);
+  }, tma.ecb);
+};
--- tma.js
+++ tma.js
@@ -1,5 +1,6 @@
 /**
  * T'MediaArt library for JavaScript.
+ * - bootstrap script - the first boot loader.
  */
 
 /**
@@ -8,195 +9,31 @@
  * This prototype provides base functions.
  * @author Takashi Toyoshima <toyoshim@gmail.com>
  */
-var tma = {};
-
-/**
- * Private prototype variables.
- */
-// Holds head tag element.
-tma._head = document.getElementsByTagName('head')[0];
-// Holds base path for this JavaScript.
-tma._base = '';
-// Holds loaded scripts' data.
-tma._scripts = {};
-// If all external scripts are loaded.
-tma.ready = false;
-// Entry point for callback ti know when all external scripts are loaded.
-tma.onload = null;
-// Additional library URL list to load initially.
-tma.extlibs = [];
-
-/**
- * Takes a log at a each level.
- * @param arguments Variable number of arguments to log
- */
-tma.log = function () {
-    console.log.apply(console, arguments);
-};
-tma.info = function () {
-    console.info.apply(console, arguments);
-};
-tma.warn = function () {
-    console.warn.apply(console, arguments);
-};
-tma.error = function () {
-    console.error.apply(console, arguments);
+var tma = {
+  base: undefined,    // Base URL to load sub resources.
+  ready: false,       // Flag to know tmalib gets ready.
+  onload: undefined,  // Callback that is fired once tmalib gets ready.
+  extlibs: []         // Holds URLs to load before the library gets ready.
 };
 
-/**
- * Gets library base directory.
- * @return path in text
- */
-tma.basePath = function () {
-    return tma._base;
-}
-/**
- * Loads JavaScript library dynamically.
- * @param src a source file URL
- * @param callback callback to invoke when the JavaScript is loaded
- * @return Promise
- */
-tma.load = function (src, callback) {
-    var script = document.createElement('script');
-    script.src = src;
-    tma._scripts[src] = script;
-    if (!callback) {
-        return new Promise(function (resolve, reject) {
-            script.onload = function () { resolve(); }
-            tma._head.appendChild(script);
-        });
-    }
-    script.onload = callback;
-    tma._head.appendChild(script);
-};
-
-/**
- * Unloads JavaScript library dynamically.
- * @param src a source file URL
- */
-tma.unload = function (src) {
-    tma._head.removeChild(tma._scripts[src]);
-    delete tma._scripts[src];
-};
-
-/**
- * Loads a shader program from external html by id.
- * @param src an external html URL
- * @param id script ID to obtain the shader program
- * @return Promise
- */
-tma.loadShader = function (src, id) {
-    return new Promise(function (resolve, reject) {
-        var frames = document.getElementsByTagName('iframe');
-        for (var i = 0; i < frames.length; ++i) {
-            if (frames[i].src == src || frames[i]._src == src) {
-                if (frames[i]._load) {
-                    resolve(frames[i].contentDocument.getElementById(id).text);
-                } else {
-                    var callback = frames[i].onload;
-                    frames[i].onload = function () {
-                        callback.apply(this);
-                        resolve(frames[i].contentDocument.getElementById(
-                                id).text);
-                    };
-                }
-                return;
-            }
-        }
-        var frame = document.createElement('iframe');
-        frame.onload = function () {
-            this._load = true;
-            resolve(this.contentDocument.getElementById(id).text); };
-        frame.src = src;
-        frame._src = src;
-        tma._head.appendChild(frame);
-    });
-};
-
-/**
- * Debug function to reload all external JavaScript files which are loaded.
- * @param callback callback to invoke when the JavaScript is reloaded
- */
-tma.reload = function (callback) {
-    // TODO: support shader reload.
-    var srcs = [];
-    for (var key in tma._scripts) {
-        srcs.push(key);
-        tma._head.removeChild(tma._scripts[key]);
-    }
-    tma._load(srcs, callback);
-};
-
-/**
- * Private load implementation to support plural source files.
- * @param srcs source file paths
- * @param callback callback to invoke when the JavaScript is loaded
- */
-tma._load = function (srcs, callback) {
-    var src = srcs.shift();
-    tma.load(this._base + src, function () {
-        if (srcs.length !== 0)
-            tma._load(srcs, callback);
-        else
-            callback();
-    });
-};
-
-/**
- * Fetches a data via XMLHttpRequest.
- * @param url a url to fetch a data
- * @param type a response type in string (optional: 'arraybuffer' is default)
- * @return a Primise
- */
-tma.fetch = function (url, type) {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = type || 'arraybuffer';
-        xhr.onload = function () {
-            if (!this.response)
-                reject();
-            else
-                resolve(this.response);
-            this.onload = null;
-        }.bind(xhr);
-        xhr.send();
-    });
-};
-
-/**
- * Initializations.
- */
-var exports = {};
-var global = window;
-(function() {
+// Starts loading sub scripts after DOM gets ready. A user may set tma.onload
+// and tma.extlibs to use additional libraries until then.
+document.addEventListener('DOMContentLoaded', function () {
+  if (tma.base === undefined) {
+    tma.base = '';
     var scripts = document.getElementsByTagName('script');
     for (var i = 0; i < scripts.length; i++) {
-        var match = scripts[i].src.match(/(^|.*\/)tma\.js$/);
-        if (match) {
-            tma._base = match[1];
-            break;
-        }
+      var match = scripts[i].src.match(/(^|.*\/)tma\.js$/);
+      if (match) {
+        tma.base = match[1];
+        break;
+      }
     }
-    var libs = [
-        'src/TmaScreen.js',
-        'src/Tma2DScreen.js',
-        'src/Tma3DScreen.js',
-        'src/TmaParticle.js',
-        'src/TmaSequencer.js',
-        'src/TmaMotionBvh.js',
-        'src/TmaModelPly.js',
-        'src/TmaModelPrimitives.js',
-        'src/TmaModelPs2Ico.js'
-    ];
-    tma._load(libs, function () {
-        tma.ready = true;
-        if (0 !== tma.extlibs.length)
-            tma._load(tma.extlibs, function () {
-                if (tma.onload)
-                    tma.onload();
-            });
-        else if (tma.onload)
-            tma.onload();
-    });
-})();
+  }
+  var core = document.createElement('script');
+  core.src = tma.base + 'src/core.js';
+  core.onload = function () {
+    tma._boot();
+  };
+  document.head.appendChild(core);
+});
