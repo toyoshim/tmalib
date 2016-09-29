8f8f7e374daaaaca0a10116d9829ca7894800595
Takashi Toyoshima <toyoshim@gmail.com>
2016-09-29 20:31:50 +0000

    MajVj crt effect: introduce more patches and examples for them

--- ext/mv/effect/crt/main.js
+++ ext/mv/effect/crt/main.js
@@ -14,14 +14,26 @@ MajVj.effect.crt = function (options) {
             this._screen.compileShader(Tma3DScreen.FRAGMENT_SHADER,
                     MajVj.effect.crt._fragmentShader));
     this._coords = this._screen.createBuffer([-1, -1, -1, 1, 1, 1, 1, -1]);
+    var patch = null;
+    if (options.patch) {
+        patch = {
+            rgb: MajVj.effect.crt._patchRgb,
+            led: MajVj.effect.crt._patchLed,
+            panel: MajVj.effect.crt._patchPanel
+        }[options.patch];
+    }
+    if (!patch)
+        patch = MajVj.effect.crt._patchRgb;
     this._patch = this._screen.createTexture(
-            MajVj.effect.crt._patchCrt, true, Tma3DScreen.FILTER_LINEAR);
+            patch, true, Tma3DScreen.FILTER_LINEAR);
 };
 
 // Shader programs.
 MajVj.effect.crt._vertexShader = null;
 MajVj.effect.crt._fragmentShader = null;
-MajVj.effect.crt._patchCrt = null;
+MajVj.effect.crt._patchRgb = null;
+MajVj.effect.crt._patchLed = null;
+MajVj.effect.crt._patchPanel = null;
 
 /**
  * Loads resources asynchronously.
@@ -31,11 +43,15 @@ MajVj.effect.crt.load = function () {
         Promise.all([
             MajVj.loadShader('effect', 'crt', 'shaders.html', 'vertex'),
             MajVj.loadShader('effect', 'crt', 'shaders.html', 'fragment'),
-            MajVj.loadImage('effect', 'crt', 'rgb.png')
+            MajVj.loadImage('effect', 'crt', 'rgb.png'),
+            MajVj.loadImage('effect', 'crt', 'led.png'),
+            MajVj.loadImage('effect', 'crt', 'panel.png')
         ]).then(function (data) {
             MajVj.effect.crt._vertexShader = data[0];
             MajVj.effect.crt._fragmentShader = data[1];
-            MajVj.effect.crt._patchCrt = data[2];
+            MajVj.effect.crt._patchRgb = data[2];
+            MajVj.effect.crt._patchLed = data[3];
+            MajVj.effect.crt._patchPanel = data[4];
             resolve();
         }, function () { reject('crt.load fails'); });
     });
