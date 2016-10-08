495db211734f5069740412903677f4496c4cfa53
Takashi Toyoshima <toyoshim@gmail.com>
2016-10-08 13:55:26 +0000

    MajVj vertexshaderart frame: support time, sound, floatSound

--- ext/mv/frame/vertexshaderart/main.js
+++ ext/mv/frame/vertexshaderart/main.js
@@ -14,12 +14,19 @@ MajVj.frame.vertexshaderart = function (options) {
                     MajVj.frame.vertexshaderart._vheader + this._vshader),
             this._screen.compileShader(Tma3DScreen.FRAGMENT_SHADER,
                     MajVj.frame.vertexshaderart._fshader));
+    this._sound = this._screen.createAlphaTexture(
+            new Uint8Array(1024 * 240), 1024, 240, false);
+    this._floatSound = this._screen.createAlphaFloatTexture(
+            new Float32Array(1024 * 240), 1024, 240, false);
     this._vertexCount = 0;
     this._vertices = null;
     this.properties = {
-        vertexCount: 1024,
-        background: [ 0.0, 0.0, 0.0, 1.0 ],
-        mode: Tma3DScreen.MODE_POINTS
+        vertexCount: options.vertexCount || 1024,
+        background: options.background || [ 0.0, 0.0, 0.0, 1.0 ],
+        mode: options.mode || Tma3DScreen.MODE_POINTS,
+        time: 0.0,
+        sound: new Uint8Array(1024),
+        floatSound: new Float32Array(1024)
     };
 };
 
@@ -27,6 +34,10 @@ MajVj.frame.vertexshaderart._vheader = ' \
         attribute float vertexId; \
         uniform float vertexCount; \
         uniform vec2 resolution; \
+        uniform float time; \
+        uniform sampler2D sound; \
+        uniform sampler2D floatSound; \
+        uniform vec4 background; \
         varying vec4 v_color;';
 
 MajVj.frame.vertexshaderart._vshader = ' \
@@ -96,7 +107,23 @@ MajVj.frame.vertexshaderart.prototype.draw = function (delta) {
     // 
     // BLEND is enabled, function is ONE,ONE_MINUS_SRC_ALPHA,
     // DEPTH_TEST is enabled.
-    
+
+    this.properties.time += delta;
+
+    var sound = this._sound.data;
+    var floatSound = this._floatSound.data;
+    for (var i = 1024 * 240 - 1; i >= 1024; --i) {
+        var j = i - 1024;
+        sound[i] = sound[j];
+        floatSound[i] = floatSound[j];
+    }
+    for (; i >= 0; --i) {
+        sound[i] = this.properties.sound[i];
+        floatSound[i] = this.properties.floatSound[i];
+    }
+    this._sound.update(sound);
+    this._floatSound.update(floatSound);
+
     if (this.properties.vertexCount != this._vertexCount)
         this._prepareVertices();
 
@@ -116,9 +143,9 @@ MajVj.frame.vertexshaderart.prototype.draw = function (delta) {
     this._program.setUniformVector('resolution', [ this._width, this._height ]);
     // TODO: mouse
     // TODO: touch
-    // TODO: time
-    // TODO: sound
-    // TODO: floatSound
+    this._program.setUniformVector('time', [ this.properties.time / 1000 ]);
+    this._program.setTexture('sound', this._sound);
+    this._program.setTexture('floatSound', this._floatSound);
     // TODO: soundRes
     this._program.setUniformVector('background', this.properties.background);
 
