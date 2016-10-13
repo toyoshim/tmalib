c028070f3ba62e979f236ae51987e8078e1dc955
Takashi Toyoshima <toyoshim@gmail.com>
2016-10-13 15:38:43 +0000

    MajVj automator: add more mode

--- ext/mv/misc/automator/main.js
+++ ext/mv/misc/automator/main.js
@@ -5,15 +5,34 @@
  */
 MajVj.misc.automator = function (options) {
     this._options = options;
-    this._t = 0;
+    this._mv = options.mv;
+    this._t = 0.0;
+    this._delta = 0.0;
+    this._v = [0.0];
+    this._o = [];
     this._random = Math.random;
-    this._last = 0;
+    this._sin = Math.sin;
+    this._last = 0.0;
     switch (this._options.type) {
     case 'randomly_gated_random':
         this._generate = this._randomlyGatedRandom.bind(this);
         break;
+    case 'sine':
+        this._generate = this._sine.bind(this);
+        break;
+    case 'vibration':
+        this._generate = this._vibration.bind(this);
+        break;
+    case 'nested':
+        for (var i = 0; i < this._options.options; ++i) {
+            var opt = this._options.options[i];
+            opt.mv = opt.mv || this._mv;
+            this._o[i] = this._mv.create('misc', 'automator', opt);
+        }
+        this._generate = this._nested.bind(this);
+        break;
     default:
-        this._generate = function() {};
+        this._generate = function() { return 0; };
         break;
     }
 };
@@ -31,14 +50,16 @@ MajVj.misc.automator.load = function () {
 /**
  * Generates automated value.
  * @param delta delta time from the last call (in msec)
- * @return generated value
+ * @return generated value from 0 to 1
  */
 MajVj.misc.automator.prototype.generate = function (delta) {
     this._t += delta;
+    this._delta = delta;
     return this._generate();
 };
 
 /**
+ * Generates automated value for type 'randomly_gated_random'.
  * options.rate: how often should it be updated (0: never - 1: always)
  * options.offset: offset value that would be applied in |gate_rate| rate
  * options.volume: offset + volume * Math.random() is applied if it isn't gated
@@ -55,4 +76,46 @@ MajVj.misc.automator.prototype._randomlyGatedRandom = function () {
     else
         this._last = opt.offset + opt.volume * rnd();
     return this._last;
+};
+
+/**
+ * Generates automated value for type 'sine'.
+ * options.rate: how often should it be updated (0: never -)
+ * options.offset: offset value
+ * options.volume: offset + volume * Math.sin() is applied
+ */
+MajVj.misc.automator.prototype._sine = function () {
+    var opt = this._options;
+    return opt.offset + this._sin(this._t * opt.rate) * opt.volume;
+};
+
+/**
+ * Generates automated value for type 'vibration'.
+ * options.rate: how often should it be updated (0: never -)
+ * options.input[options.index]: input value
+ * options.threshold: ignore if value is less than threshold
+ * options.offset: offset value
+ * options.volume: sine is mutiplied by volume-ish value
+ * options.tension: how quicktly value affects input (0: never - 1: immediately)
+ */
+MajVj.misc.automator.prototype._vibration = function () {
+    var opt = this._options;
+    var v = opt.input[opt.index];
+    if (v < opt.threshold)
+        v = 0;
+    var diff = v - this._v[0];
+    this._v[0] += diff * opt.tension;
+    var t = this._t * opt.rate;
+    return opt.offset + this._sin(t) * opt.volume * this._v[0];
+};
+
+/**
+ * Generates automated value for type 'nested'.
+ * Everything depend on nested automators.
+ */
+MajVj.misc.automator.prototype._nested = function () {
+    var v = 0;
+    for (var i = 0; i < this._o.length; ++i)
+        v += this._o[i].generate(this._delta);
+    return v;
 };
