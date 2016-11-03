e309609ea958d615e708981eeea94f6bb14d37dc
Takashi Toyoshima <toyoshim@gmail.com>
2016-11-03 08:36:03 +0000

    MajVj noisybase scene: style fix

--- ext/mv/scene/noisybase/main.js
+++ ext/mv/scene/noisybase/main.js
@@ -15,17 +15,17 @@ MajVj.scene.noisybase = function (options) {
     noise1: 0.0,
     noise2: 0.0,
   };
-	this._fbo = this._mv.screen().createFrameBuffer();
-	this._frame = this._mv.create('frame', 'sandbox', { id: '14373.1' });
-	this._effect = this._mv.create('effect', 'noise', {
-	    enable: [
-	      'scanline', 'raster', 'color', 'noise', 'slitscan', 'adjust',
-	      'tube', 'film'
-	    ]
-	});
-	this._effect.properties.noise_level[2] = 0;
-	this._effect.properties.raster_level = 0;
-	this._effect.properties.scanline_velocity = 0;
+  this._fbo = this._mv.screen().createFrameBuffer();
+  this._frame = this._mv.create('frame', 'sandbox', { id: '14373.1' });
+  this._effect = this._mv.create('effect', 'noise', {
+      enable: [
+        'scanline', 'raster', 'color', 'noise', 'slitscan', 'adjust',
+        'tube', 'film'
+      ]
+  });
+  this._effect.properties.noise_level[2] = 0;
+  this._effect.properties.raster_level = 0;
+  this._effect.properties.scanline_velocity = 0;
 };
 
 /**
@@ -34,24 +34,24 @@ MajVj.scene.noisybase = function (options) {
  */
 MajVj.scene.noisybase.prototype.draw = function (delta) {
   var screen = this._fbo.bind();
-	this._mv.screen().fillColor(0.0, 0.0, 0.0, 1.0);
-	this._frame.draw(delta);
-	screen.bind();
-	this._mv.screen().fillColor(0.0, 0.0, 0.0, 1.0);
-	var rgb = this.properties.rgb / 1000;
-	this._effect.properties.color_shift[0] = -rgb;
-	this._effect.properties.color_shift[2] = rgb;
-	var c = 1 - this.properties.color / 127;
-	this._effect.properties.color_level = [c, c, c];
-	this._effect.properties.noise_level[0] = this.properties.noise1 / 128;
-	this._effect.properties.noise_level[1] = this.properties.noise2 / 63;
-	this._effect.properties.slitscan_size = 1 + this.properties.slitter;
-	this._effect.properties.tube_adjust[0] = 1 - this.properties.tube / 127;
-	this._effect.properties.tube_adjust[1] = 16 + this.properties.tube / 4;
-	this._effect.properties.film_lines = (this.properties.film / 25) | 0;
-	this._effect.properties.film_flash = this.properties.film / 96;
-	var multi = 1 + (this.properties.multi / 16) | 0;
-	this._effect.properties.adjust_repeat[0] = multi;
-	this._effect.properties.adjust_repeat[1] = multi;
-	this._effect.draw(delta, this._fbo.texture);
+  this._mv.screen().fillColor(0.0, 0.0, 0.0, 1.0);
+  this._frame.draw(delta);
+  screen.bind();
+  this._mv.screen().fillColor(0.0, 0.0, 0.0, 1.0);
+  var rgb = this.properties.rgb / 1000;
+  this._effect.properties.color_shift[0] = -rgb;
+  this._effect.properties.color_shift[2] = rgb;
+  var c = 1 - this.properties.color / 127;
+  this._effect.properties.color_level = [c, c, c];
+  this._effect.properties.noise_level[0] = this.properties.noise1 / 128;
+  this._effect.properties.noise_level[1] = this.properties.noise2 / 63;
+  this._effect.properties.slitscan_size = 1 + this.properties.slitter;
+  this._effect.properties.tube_adjust[0] = 1 - this.properties.tube / 127;
+  this._effect.properties.tube_adjust[1] = 16 + this.properties.tube / 4;
+  this._effect.properties.film_lines = (this.properties.film / 25) | 0;
+  this._effect.properties.film_flash = this.properties.film / 96;
+  var multi = 1 + (this.properties.multi / 16) | 0;
+  this._effect.properties.adjust_repeat[0] = multi;
+  this._effect.properties.adjust_repeat[1] = multi;
+  this._effect.draw(delta, this._fbo.texture);
 };
