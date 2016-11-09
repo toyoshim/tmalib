28ffae41d4f410777a9a940c2820655b5846ee0d
Takashi Toyoshima <toyoshim@gmail.com>
2016-11-10 06:40:43 +0900

    MajVj photoframe frame: support property control

--- ext/mv/frame/photoframe/main.js
+++ ext/mv/frame/photoframe/main.js
@@ -6,20 +6,16 @@
 MajVj.frame.photoframe = function(options) {
   this.properties = {};
   this._data = [];
-  var mv = options.mv;
+  this._mv = options.mv;
   var data = options.data || [];
   for (var i = 0; i < data.length; ++i) {
     this._data[i] = {
-      frame: mv.create(
+      frame: this._mv.create(
           'frame', data[i].type, { url: data[i].url, rate: data[i].rate }),
-      x: data[i].x ?
-          mv.create('misc', 'sequencer', { sequence: data[i].x }) : null,
-      y: data[i].y ?
-          mv.create('misc', 'sequencer', { sequence: data[i].y }) : null,
-      scale: data[i].scale ?
-          mv.create('misc', 'sequencer', { sequence: data[i].scale }) : null,
-      volume: data[i].volume ?
-          mv.create('misc', 'sequencer', { sequence: data[i].volume }) : null
+      x: this._createAutomation(data[i].x, 0),
+      y: this._createAutomation(data[i].y, 0),
+      scale: this._createAutomation(data[i].scale, 1),
+      volume: this._createAutomation(data[i].volume, 1)
     };
   }
 };
@@ -48,15 +44,24 @@ MajVj.frame.photoframe.prototype.onresize = function(aspect) {
 MajVj.frame.photoframe.prototype.draw = function(delta) {
   for (var i = 0; i < this._data.length; ++i) {
     var data = this._data[i];
-    data.frame.properties.scroll[0] =
-        data.x ? data.x.generate(delta) : 0;
-    data.frame.properties.scroll[1] =
-        data.y ? data.y.generate(delta) : 0;
-    data.frame.properties.scale =
-        data.scale ? data.scale.generate(delta) : 1;
-    data.frame.properties.volume =
-        data.volume ? data.volume.generate(delta) : 1;
+    data.frame.properties.scroll[0] = data.x.generate(delta);
+    data.frame.properties.scroll[1] = data.y.generate(delta);
+    data.frame.properties.scale =  data.scale.generate(delta);
+    data.frame.properties.volume = data.volume.generate(delta);
     if (data.frame.properties.volume)
       data.frame.draw(delta);
   }
+};
+
+MajVj.frame.photoframe.prototype._createAutomation =
+    function(data, fallbackConstant) {
+  if (!data)
+    return { generate: function (delta) { return fallbackConstant; } };
+  if (data[0].type != 'properties')
+    return this._mv.create('misc', 'sequencer', { sequence: data });
+  return {
+    generate: (delta) => {
+      return data[0].offset + this.properties[data[0].name] * data[0].scale;
+    }
+  };
 };
