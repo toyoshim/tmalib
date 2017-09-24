fde84f7dcfd581f1ef9e95e804d340c55d5b8cd7
Takashi Toyoshima <toyoshim@gmail.com>
2017-09-24 16:49:54 +0000

    MajVj nicofarre3d frame, harrier module: add scale option

--- ext/mv/frame/nicofarre3d/harrier.js
+++ ext/mv/frame/nicofarre3d/harrier.js
@@ -5,6 +5,7 @@
 MajVj.frame.nicofarre3d.modules.harrier = function (options) {
   this.properties = options.properties;
   this.properties.harrier = 0.0;
+  this.properties.scale = options.scale || 1.0;
   this._zspeed = options.zspeed || 10.0;
   this._xspeed = options.xspeed || 0.00;
   this._zinterval = options.zinterval || 50.0;
@@ -32,12 +33,13 @@ MajVj.frame.nicofarre3d.modules.harrier.prototype.draw = function (api) {
   this._t += api.delta;
   api.setAlphaMode(true, api.gl.ONE, api.gl.ONE);
   api.color = [1.0, 1.0, 1.0, 1.0];
-  var s = 100000;
-  var l = 1000000;
+  var s = 100000 * this.properties.scale;
+  var d = 10000 * this.properties.scale;
+  var l = 1000000 * this.properties.scale;
   var c = 0;
   var t = this._t / 1000 * this._zspeed;
-  var y = 5000 * (1 + this.properties.harrier * 10);
-  for (var z = -s; z < s; z += 10000) {
+  var y = 5000 * (1 + this.properties.harrier * 10) * this.properties.scale;
+  for (var z = -s; z < s; z += d) {
     c = (1.0 + Math.sin(t + z / s * this._zinterval)) / 2.0;
     api.color =
         [this._color[0] * c, this._color[1] * c, this._color[2] * c, 1.0];
@@ -46,7 +48,7 @@ MajVj.frame.nicofarre3d.modules.harrier.prototype.draw = function (api) {
       api.drawLine([-l, y, z], [l, y, z]);
   }
   t = this._t / 1000 * this._xspeed;
-  for (var x = -s; x < s; x += 10000) {
+  for (var x = -s; x < s; x += d) {
     c = (1.0 + Math.sin(t)) / 2.0;
     c = (1.0 + Math.sin(t + x / s * this._xinterval)) / 2.0;
     api.color =
